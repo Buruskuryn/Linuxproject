04c1c12e44ffb96fd6e5990b337bb58e27075ce3
sicatombe <alexandre.laurent.mail@gmail.com>
2017-05-18 20:45:12 +0200

    Même code avec des commentaires en plus

--- main.c
+++ main.c
@@ -7,19 +7,19 @@
 #include <signal.h>
 #include <dirent.h>
 
-#define couleur(param) printf("\033[%sm",param)
+#define couleur(param) printf("\033[%sm",param) //Cette ligne permet de modifier facilement la couleur dans le programme.
 
 struct dirent* fichier;
 
-void clearScreen() 
+void clearScreen()  //Cette fonction a pour but d'"effacer" le contenu de l'écran en effectuant un scroll.
 {
 	printf("\033[2J\033[1;1H");
 	
 }
 
-void renommer(char* arg1,char* arg2)
+void renommer(char* arg1,char* arg2) 
 {
-	couleur("31");
+	couleur("31"); //Tous les commentaire qui sont toujours pour cette fonction des erreurs s'afficheront en rouge
 	if(strlen(arg1)&&strlen(arg2))
 	{
 		if(!strcmp(arg1,arg2))
@@ -41,19 +41,19 @@ void supprimer(char* arg)
 
 void diviserSaisie(char* saisie,char arg1[],char arg2[])
 {
-	char cmd[50];
-	arg1[0]='\0';
+	char cmd[50]; //Pour séparer la saisie de la premiere partie de saisie.
+	arg1[0]='\0'; //on initialise à zéro les arguments.
 	arg2[0]='\0';
 	unsigned int i=0;
-	while((i<strlen(saisie)-1)&&(saisie[i]!=' ')&&(saisie[i]!='\n'))
+	while((i<strlen(saisie)-1)&&(saisie[i]!=' ')&&(saisie[i]!='\n')) //On remplit cmd par le premier terme de la saisie.
 	{
 		cmd[i]=saisie[i];
 		i++;
 	}
-	cmd[i]='\0';
+	cmd[i]='\0'; // On indique que le terme est terminé.
 	i++;
 	unsigned int j=0;
-	while((i+j<strlen(saisie)-1)&&(saisie[i+j]!=' ')&&(saisie[i+j]!='\n'))
+	while((i+j<strlen(saisie)-1)&&(saisie[i+j]!=' ')&&(saisie[i+j]!='\n')) //on fait la même chose pour le premier argument...
 	{
 		arg1[j]=saisie[i+j];
 		j++;
@@ -61,7 +61,7 @@ void diviserSaisie(char* saisie,char arg1[],char arg2[])
 	arg1[j]='\0';
 	j++;
 	unsigned int k=0;
-	while((i+j+k<strlen(saisie)-1)&&(saisie[i+j+k]!=' ')&&(saisie[i+j+k]!='\n'))
+	while((i+j+k<strlen(saisie)-1)&&(saisie[i+j+k]!=' ')&&(saisie[i+j+k]!='\n')) //... et pour le second.
 	{
 		arg2[k]=saisie[i+j+k];
 		k++;
@@ -72,7 +72,7 @@ void diviserSaisie(char* saisie,char arg1[],char arg2[])
 
 void help()
 {
-	couleur("32");
+	couleur("32"); //Le texte s'affichera en vert.
 	printf("---Liste des commandes disponibles---\n");
 	printf("changePrompt : permet d'activer ou de désactiver l'affichage du chemin du répertoire courant dans le prompt.\n");
 	printf("cd <arg> : permet de se déplacer vers le répertoire arg.\n");
@@ -88,11 +88,11 @@ void help()
 void Lister_RepC()
 {
 	char repC[50];
-	getcwd(repC,50);
-	printf("Contenu du répertoire courant (%s)\n",repC);
+	getcwd(repC,50); //On récupère le chemin du répertoire courant dans une chaîne de caractère.
+	printf("Contenu du répertoire courant (%s) :\n",repC);
 	couleur("0");
 	DIR* rep = opendir("."); 
-	if (rep)   
+	if (rep)
 	{
 		while ((fichier = readdir(rep)) != NULL)
 		if (strcmp(fichier->d_name,".") && strcmp(fichier->d_name,".."))
@@ -119,7 +119,7 @@ void Lister(char *direc)
 		}
 		printf("\n");
 	}
-	else
+	else //Si rep est nul, cela veut dire que le répertoire n'a pas été trouvé.
 	{
 		couleur("31");
 		printf("Opération impossible : répertoire %s n'existe pas.\n",direc);
@@ -136,10 +136,10 @@ void cd(char *direc)
 		chdir(direc);
 		closedir(rep);
 	}
-	else
+	else //De la même façon que pour la fonction Lister, si rep est nul, cela veut dire que le répertoire n'existe pas.
 	{
 		couleur("31");
-		printf("Impossible d'aller vers %s : ce répertoire n'existe pas\n",direc);
+		printf("Impossible d'aller vers %s : ce répertoire n'existe pas.\n",direc);
 		couleur("0");
 	}
 }
@@ -152,7 +152,7 @@ int main()
 	char* buf=(char*)malloc(50*sizeof(char)); //Correspond au chemin du repertoire courant
 	if(!buf)
 	{
-		printf("Erreur malloc");
+		printf("Erreur malloc.");
 		exit(-1);
 	}
 	char saisie[50]; //Correspond à la saisie de l'utilisateur
@@ -166,15 +166,15 @@ int main()
 		else{buf[0]='\0';}
 		saisie[0]='\0'; //mise à zéro de la saisie
 		couleur("33");
-		printf("%s:",buf);
+		printf("%s:",buf); //On affiche le contenu de buf qui correspond à un prompt.
 		couleur("0");
 		//scanf("%s",saisie);
-		fgets(saisie,sizeof(saisie),stdin);
-		diviserSaisie(saisie,arg1,arg2);
+		fgets(saisie,sizeof(saisie),stdin); //Mieux qu'un scanf car le scanf s'arrête aux espaces.
+		diviserSaisie(saisie,arg1,arg2); //permet d'interpréter la saisie en un fonction et des potentiels arguments
 				
-		if(!strcmp(saisie,"exit"))
+		if(!strcmp(saisie,"exit")) //strcmp renvoie 0 quand les deux chaînes sont identiques.
 		{
-			q=1;
+			q=1; //Quand q=1 on sort de la boucle et le programme s'arrête.
 		}
 		else if(!strcmp(saisie,"changePrompt"))
 		{
@@ -205,7 +205,7 @@ int main()
 		{
 			help();
 		}
-		else
+		else //Si aucune des commandes ne correspond à la saisie, ça veut dire que la saisie est fausse.
 		{
 			couleur("31");
 			printf("Commande introuvable, saisissez help pour afficher la liste des commandes disponibles.\n");
@@ -213,6 +213,6 @@ int main()
 			
 		}
 	}
-	free(buf);
+	free(buf); //On doit libérer la mémoire car il s'agissait d'une allocation dynamique.
 	return 0;
 }
